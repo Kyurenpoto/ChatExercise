ffedb3c6350cbc2a0685afcdbb196df88e73cbf4
Kyurenpoto <heal9179@daum.net>
2019-01-25 05:24:44 +0900

    Issue #8 - Update test script
    Edit class filter to exclude [ChatExercise.Properties.*] modules

--- test.ps1
+++ test.ps1
@@ -35,4 +35,4 @@ Switch ($CONFIGURATION) {
     }
 }
 
-& OpenCover.Console.exe -register:user -target:"$Target" -targetargs:"$TargetArgs" -output:"$OUTPUT" -filter:"+[ChatExercise*]* -[ChatExercise*]*Designer -[ChatExercise.Test*]*"
+& OpenCover.Console.exe -register:user -target:"$Target" -targetargs:"$TargetArgs" -output:"$OUTPUT" -filter:"+[ChatExercise*]* -[ChatExercise*]Properties.* -[ChatExercise.Test*]*"
