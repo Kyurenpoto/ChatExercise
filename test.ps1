f0766b31f0852cbee4248369a1993b61ba090618
Kyurenpoto <heal9179@daum.net>
2019-01-25 01:28:42 +0900

    Issue #8 - Update test script
    Add preparing by reference to
    [https://help.appveyor.com/discussions/problems/1896-vstestconsole-cant-find-logger].

--- test.ps1
+++ test.ps1
@@ -3,15 +3,18 @@ $CONFIGURATION = $args[1]
 $OUTPUT = $args[2]
 
 $Target = $null
+$Ext = $null
 $TargetArgs = $null
 
 Switch ($IMAGE) {
     'Visual Studio 2019 Preview' {
         $Target = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+        $Ext = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE\CommonExtensions\Microsoft\TestWindow\Extensions"
         Break
     }
     'Visual Studio 2017' {
         $Target = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+        $Ext = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\Extensions"
         Break
     }
     default {
@@ -36,4 +39,5 @@ Switch ($CONFIGURATION) {
 }
 
 & OpenCover.Console.exe -register:user -target:"$Target" -targetargs:"$TargetArgs" -output:"$OUTPUT"
+& copy "$Ext\AppVeyor*" "$Ext"
 & $Target $TargetArgs /Logger:Appveyor /Parallel
